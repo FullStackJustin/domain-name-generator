8cd638d47d2ada0ecb8f658bc82af9be7f714a14
FullStackJustin <ju.tay.lor2019@gmail.com>
2022-03-30 18:35:16 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+function domainUrl() {
+    let pronoun = ['the', 'allTime', 'our', ];
+    let adj = ['greatest', 'worst', 'unexplainable'];
+    let noun = ['hope', 'adventure', 'mission']
+    let dot = ['.com', '.net', '.edu']
+
+    
+}
+
+
+
+
+
+
+
+
+console.log("domainUrl")
