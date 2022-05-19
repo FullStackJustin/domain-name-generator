48e3b51b30b28b8c545bb7a2960a31c8a7410954
FullStackJustin <ju.tay.lor2019@gmail.com>
2022-05-19 20:30:48 +0000

    third attempt

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-// function domainUrl() {
-//     let pronoun = ['the', 'allTime', 'our', ];
-//     let adj = ['greatest', 'worst', 'unexplainable'];
-//     let noun = ['hope', 'adventure', 'mission']
-//     let dot = ['.com', '.net', '.edu']
 
-    
-// }
-
-
-
-
+function domainUrl() {
+    var pronoun = ['The', 'Alltime', 'Our',];
+    var adj = ['Greatest', 'Worst', 'Unexplainable'];
+    var noun = ['Hope', 'Adventure', 'Mission'];
+    var dot = ['.com', '.net', '.edu'];
+    var insert = document.getElementById("text")
+    insert.innerHTML = 
+    pronoun[Math.floor(Math.random() * pronoun.length)] +
+    adj[Math.floor(Math.random() *adj.length)] +
+    noun[Math.floor(Math.random() *noun.length)] +
+    dot[Math.floor(Math.random() *dot.length)];
+}
 
 
 
